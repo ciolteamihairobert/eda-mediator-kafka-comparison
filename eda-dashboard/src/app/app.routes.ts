1bdb5e4942ef35f05c01d073e87daf98a901ec26
robert.cioltea <robertcioltea@gmail.com>
2026-01-10 15:05:34 +0200

    Add the home topbar and the shell of the main page without functionalities

--- eda-dashboard/src/app/app.routes.ts
+++ eda-dashboard/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './core/layout/shell/shell.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+const DummyLoginComponent = HomeComponent;
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'login', component: DummyLoginComponent },
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
